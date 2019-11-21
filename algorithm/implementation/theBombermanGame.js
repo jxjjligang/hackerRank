a6df6028b589457aa254f0a9c483a8c0220abdf9
Gang Li <jxjjligang@gmail.com>
2019-11-21 11:32:46 -0700

    Accomplish [The Bomberman Game] challenge.

--- algorithm/implementation/theBombermanGame.js
+++ algorithm/implementation/theBombermanGame.js
@@ -4,6 +4,9 @@ main();
 
 // Complete the bomberMan function below.
 function bomberMan(n, grid) {
+    const EMPTY = '.', BOMB = 'O', POSITIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const GRID_ROWS = grid.length, GRID_COLUMNS = grid[0].length;
+
     function decreaseTime(gridArr) {
         for (let i = 0; i < GRID_ROWS; i++) {
             let row = gridArr[i];
@@ -47,26 +50,50 @@ function bomberMan(n, grid) {
         }
     }
 
-    const EMPTY = '.', BOMB = 'O', POSITIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    const GRID_ROWS = grid.length, GRID_COLUMNS = grid[0].length;
-    let gridArr = grid.map(row => row.split('').map(c => (c === BOMB ? 3 : c)));
-    let secondsElapsed = 0;
-    while (n > 0) {
-        n--;
-        secondsElapsed++;
+    function outputResult(secondsElapsed, gridArr) {
+        console.log(`${secondsElapsed} seconds passed.`);
 
-        decreaseTime(gridArr);
-        detnote(gridArr);
-        if (secondsElapsed % 2 === 0)
-            plantBomb(gridArr);
+        let tmpGridArr = gridArr.map(row => row.map(c => (c === 3 || c === 2 || c === 1) ? BOMB : c));
+        tmpGridArr = tmpGridArr.map(row => row.join(''));
+        console.log(tmpGridArr.join(''));
+        console.log();
     }
 
-    gridArr = gridArr.map(row => row.map(c => (c === 3 || c === 2 || c === 1) ? BOMB : c));
-    return gridArr.map(row => row.join(''));
+    function getGridAfter(n, grid) {
+        let gridArr = grid.map(row => row.split('').map(c => (c === BOMB ? 3 : c)));
+        let secondsElapsed = 0;
+        while (n > 0) {
+            n--;
+            secondsElapsed++;
+
+            decreaseTime(gridArr);
+            detnote(gridArr);
+            if (secondsElapsed % 2 === 0)
+                plantBomb(gridArr);
+
+            // outputResult(secondsElapsed, gridArr);
+        }
+
+        gridArr = gridArr.map(row => row.map(c => (c === 3 || c === 2 || c === 1) ? BOMB : c));
+        return gridArr.map(row => row.join(''));
+    }
+
+    if (n <= 1)
+        return grid;
+    else {
+        if (n % 2 === 0)
+            return grid.map(row => BOMB.repeat(GRID_COLUMNS));
+        else {
+            let remainder = n % 4;
+            return getGridAfter(remainder === 1 ? 5 : remainder, grid);
+        }
+    }
 }
 
 function main() {
-    let inputs = [`6 7 5
+    let inputs = [`1 199 181054341
+    O..OO........O..O........OO.O.OO.OO...O.....OOO...OO.O..OOOOO...O.O..O..O.O..OOO..O..O..O....O...O....O...O..O..O....O.O.O.O.....O.....OOOO..O......O.O.....OOO....OO....OO....O.O...O..OO....OO..O...O`,
+        `6 7 5
     .......
     ...O.O.
     ....O..
@@ -80,7 +107,7 @@ function main() {
     .......
     OO.....
     OO.....`];
-    for (let i = 1; i < 2; i++) {    // inputs.length
+    for (let i = 0; i < 1; i++) {    // inputs.length
         let input = inputs[i], lines = input.split('\n').map(s => s.trim()), index = 0;
         const rcn = lines[index++].split(' '), r = parseInt(rcn[0], 10), c = parseInt(rcn[1], 10), n = parseInt(rcn[2], 10);
 
