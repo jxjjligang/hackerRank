3755ef81cb71ef5c9ed7b3740f3ce9f2a170a3dc
Gang Li <jxjjligang@gmail.com>
2019-11-20 22:28:03 -0700

    Accomplish [The Bomberman Game] challenge partially, stll need to oberserve the pattern to improve performance.

--- algorithm/implementation/theBombermanGame.js
+++ algorithm/implementation/theBombermanGame.js
@@ -4,37 +4,65 @@ main();
 
 // Complete the bomberMan function below.
 function bomberMan(n, grid) {
-    const EMPTY = '.', BOMB = 'O', POSITIONS = [[0, 0], [-1, 0], [1, 0], [0, -1], [0, 1]];
+    function decreaseTime(gridArr) {
+        for (let i = 0; i < GRID_ROWS; i++) {
+            let row = gridArr[i];
+            for (let j = 0; j < GRID_COLUMNS; j++) {
+                let char = row[j];
+                if (char === 3 || char === 2 || char === 1)
+                    row[j]--;
 
-    if (n <= 1)
-        return grid;
-    else {  // n > 1
-        let gridAllBombs = [];
-        for (let i = 0; i < grid.length; i++)
-            gridAllBombs.push(grid[i].slice().replace(/\./g, BOMB).split(''));
+                char = row[j];  // updated cell value
 
-        if (n % 2 === 0)
-            return gridAllBombs.map(row => row.join(''));
-        else {
-            if ((n - 1) % 4 === 0)
-                return grid;
-            else {
-                for (let i = 0; i < grid.length; i++) {
-                    let row = grid[i];
-                    for (let j = 0; j < row.length; j++) {
-                        let char = row[j];
-                        if (char === BOMB) {
-                            for (let position of POSITIONS) {
-                                if (gridAllBombs[i + position[0]] !== undefined && gridAllBombs[i + position[0]][j + position[1]] !== undefined)
-                                    gridAllBombs[i + position[0]][j + position[1]] = EMPTY;
-                            }
-                        }
-                    }
-                }
-                return gridAllBombs.map(row => row.join(''));
             }
         }
     }
+
+    function detnote(gridArr) {
+        for (let i = 0; i < GRID_ROWS; i++) {
+            let row = gridArr[i];
+            for (let j = 0; j < GRID_COLUMNS; j++) {
+                let char = row[j];
+                if (char === 0) {   // bomb detonate
+                    let jj = j;
+                    for (let position of POSITIONS) {
+                        row[j] = EMPTY;
+                        let newI = i + position[0], newJ = j + position[1];
+                        if (newI >= 0 && newI < GRID_ROWS && newJ >= 0 && newJ < GRID_COLUMNS && gridArr[newI][newJ] !== 0)
+                            gridArr[newI][newJ] = EMPTY;
+                    }
+                }   // if (char === 0) {
+            }       // for (let j = 0;
+        }           //  for (let i = 0;
+    }
+
+    function plantBomb(gridArr) {
+        for (let i = 0; i < GRID_ROWS; i++) {
+            let row = gridArr[i];
+            for (let j = 0; j < GRID_COLUMNS; j++) {
+                let char = row[j];
+                if (char === EMPTY)
+                    row[j] = 3;
+            }
+        }
+    }
+
+    const EMPTY = '.', BOMB = 'O', POSITIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+    const GRID_ROWS = grid.length, GRID_COLUMNS = grid[0].length;
+    let gridArr = grid.map(row => row.split('').map(c => (c === BOMB ? 3 : c)));
+    let secondsElapsed = 0;
+    while (n > 0) {
+        n--;
+        secondsElapsed++;
+
+        decreaseTime(gridArr);
+        detnote(gridArr);
+        if (secondsElapsed % 2 === 0)
+            plantBomb(gridArr);
+    }
+
+    gridArr = gridArr.map(row => row.map(c => (c === 3 || c === 2 || c === 1) ? BOMB : c));
+    return gridArr.map(row => row.join(''));
 }
 
 function main() {
@@ -52,7 +80,7 @@ function main() {
     .......
     OO.....
     OO.....`];
-    for (let i = 0; i < 1; i++) {    // inputs.length
+    for (let i = 1; i < 2; i++) {    // inputs.length
         let input = inputs[i], lines = input.split('\n').map(s => s.trim()), index = 0;
         const rcn = lines[index++].split(' '), r = parseInt(rcn[0], 10), c = parseInt(rcn[1], 10), n = parseInt(rcn[2], 10);
 
