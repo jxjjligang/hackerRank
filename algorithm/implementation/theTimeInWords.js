2e686302b47d4e5705372233a2df896f28274758
Gang Li <jxjjligang@gmail.com>
2019-11-24 16:19:36 -0700

    Accomplish [The Time in Words] challenge.

--- algorithm/implementation/theTimeInWords.js
+++ algorithm/implementation/theTimeInWords.js
@@ -4,7 +4,29 @@ main();
 
 // Complete the timeInWords function below.
 function timeInWords(h, m) {
-    return 'abc';
+    const numberToWord = new Map([[1, 'one'], [2, 'two'], [3, 'three'], [4, 'four'], [5, 'five'], [6, 'six'], [7, 'seven'], [8, 'eight'], [9, 'nine'], [10, 'ten'],
+    [11, 'eleven'], [12, 'twelve'], [13, 'thirteen'], [14, 'fourteen'], [15, 'quarter'], [16, 'sixteen'], [17, 'seventeen'], [18, 'eighteen'], [19, 'nineteen'], [20, 'twenty']]);
+    const O_CLOCK = "o' clock";
+
+    let word;
+    if (m === 0)
+        word = `${numberToWord.get(h)} ${O_CLOCK}`;
+    else if (m === 30)
+        word = `half past ${numberToWord.get(h)}`;
+    else {
+        let isPast = (m < 30 ? true : false), minute = (isPast ? m : (60 - m)), hour = (isPast ? h : (h + 1) % 12);
+        let minuteWord = ((minute <= 20) ? numberToWord.get(minute) : `twenty ${numberToWord.get(minute - 20)}`);
+        if (minute != 15) {
+            minuteWord += ` minute`;
+            if (minute !== 1)
+                minuteWord += 's';
+        };
+        let pastOrTo = isPast ? 'past' : 'to';
+
+        word = `${minuteWord} ${pastOrTo} ${numberToWord.get(hour)}`;
+    }
+
+    return word;
 }
 
 function main() {
